449171a8ffa8ba40f5d537ba75fa6be147126725
山根海度 <yamanekaito@mca.itabs1.kn.home.ne.jp>
2022-05-20 19:07:49 +0900

    create main.py

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,18 @@
+import json
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+ 
+file = open('ex.json','r')
+info = json.load(file)
+
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID = info['USER_ID']
+    messages = TextSendMessage(text= "お久しぶりです \n　チャンネル名が変更しました")
+    line_bot_api.push_message(USER_ID,messages=messages)
+
+if __name__ =="__main__":
+    main();
+
